6bbb9a6430f645b4ca6d25d24bd12f09ee0d2b0a
Zach Gray <zach@zachgray.io>
2018-04-29 23:02:07 -0700

    replace scripts with a more powerful combined script

--- run_xcode.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-docker build -t sts-app . && docker run --rm -v ${PWD}:/usr/src \
-    --entrypoint /usr/bin/swift \
-    sts-app \
-    package generate-xcodeproj
-open STSProject.xcodeproj
